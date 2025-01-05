ded8b65bfbb1de506bfd2a3f74a1c7a83fbffc56
JOEY <0706zengyan@gmail.com>
2025-01-05 10:33:43 +0000

    修改 run.py

--- run.py
+++ run.py
@@ -10,10 +10,12 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         super().end_headers()
 
-def run_server(port=8000):
+def run_server(port=8080):
     # 获取当前目录
-    current_dir = os.getcwd()
-    
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    print(f"当前目录: {current_dir}")
+    # 切换到当前目录
+    os.chdir(current_dir)
     # 创建服务器
     handler = MyHttpRequestHandler
     
@@ -23,20 +25,15 @@ def run_server(port=8000):
             print(f"启动服务器在: {server_url}")
             print("按 Ctrl+C 停止服务器")
             
-            # 自动在默认浏览器中打开
-            webbrowser.open(server_url)
-            
             # 开始服务
             httpd.serve_forever()
             
     except OSError as e:
         if e.errno == 98:  # 端口已被占用
             print(f"端口 {port} 已被占用，尝试其他端口...")
-            run_server(port + 1)
-        else:
-            raise e
+        raise e
+            
     except KeyboardInterrupt:
         print("\n服务器已停止")
-
 if __name__ == "__main__":
     run_server() 
